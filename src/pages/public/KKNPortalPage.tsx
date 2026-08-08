a3b307f9977daaac9e808112bca46210f9308dab
sannnbieber <rizkwya@gmail.com>
2026-08-08 22:12:18 +0800

    design: flatten wave and raise group photo center vertical alignment to reveal KKN banner fully

--- src/pages/public/KKNPortalPage.tsx
+++ src/pages/public/KKNPortalPage.tsx
@@ -24,7 +24,7 @@ export const KKNPortalPage: React.FC<KKNPortalPageProps> = ({ prokerList, kknTea
         style={{
           backgroundImage: 'linear-gradient(to bottom, rgba(11, 86, 101, 0.42) 0%, rgba(6, 48, 57, 0.65) 100%), url("/hero_sawah.jpg")',
           backgroundSize: 'cover',
-          backgroundPosition: 'center 78%',
+          backgroundPosition: 'center 68%',
           backgroundAttachment: 'scroll'
         }}
       >
@@ -68,12 +68,7 @@ export const KKNPortalPage: React.FC<KKNPortalPageProps> = ({ prokerList, kknTea
           </div>
         </div>
 
-        {/* Dynamic wave SVG transition */}
-        <div className="absolute bottom-0 left-0 right-0 w-full overflow-hidden leading-none z-20">
-          <svg viewBox="0 0 1200 120" preserveAspectRatio="none" className="relative block w-full h-[30px] sm:h-[45px]">
-            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="fill-slate-50"></path>
-          </svg>
-        </div>
+
       </section>
 
       {/* DETAILED INFO SECTION */}
