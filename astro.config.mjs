f924c0952114c96fbb9a36e027987c061c76a56a
sannnbieber <rizkwya@gmail.com>
2026-08-14 00:27:11 +0800

    seo: configure domain URL and sitemap integration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://www.rt35manggar.my.id',
   integrations: [
     react(),
-    tailwind()
+    tailwind(),
+    sitemap()
   ],
   vite: {
     ssr: {
